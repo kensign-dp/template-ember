6d9be8cb3da854cdcc9939405e803dbbef128389
Keith Ensign <kensign@dhapdigital.com>
2013-01-04 15:33:00 -0800

    removed plastron, embedded Ember.js

--- js/ApplicationController.js
+++ js/ApplicationController.js
@@ -1,1 +1,23 @@
+var App;
 
+$( 
+	function()
+	{
+		App = Ember.Application.create();
+		
+		App.ApplicationController = Ember.Controller.extend();
+		App.ApplicationView = Ember.View.extend({
+		  templateName: 'application'
+		});
+		
+		App.Router = Ember.Router.extend({
+		  root: Ember.Route.extend({
+		    index: Ember.Route.extend({
+		      route: '/'
+		    })
+		  })
+		})
+		
+		App.initialize(); 
+	}
+);
